15a41b5694c9a12361c3f945ceb29d2ef87fe333
fuscanet <traderdeluruguay@gmail.com>
2025-05-17 14:16:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const puppeteer = require("puppeteer");
+const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
 const app = express();
 
 app.get("/scrap", async (req, res) => {
@@ -8,9 +9,12 @@ app.get("/scrap", async (req, res) => {
 
   let browser;
   try {
-    browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"]
-    });
+browser = await puppeteer.launch({
+  args: chromium.args,
+  defaultViewport: chromium.defaultViewport,
+  executablePath: await chromium.executablePath,
+  headless: chromium.headless
+});
     const page = await browser.newPage();
     await page.setUserAgent(
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
